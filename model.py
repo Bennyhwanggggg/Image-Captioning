af518f816e7e513f7ef4a65d9b7d4d2f1b46f044
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2020-07-26 14:34:57 +1000

    init

---
+++ model.py
@@ -0,0 +1,47 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+
+
+class EncoderCNN(nn.Module):
+    def __init__(self, embed_size):
+        super(EncoderCNN, self).__init__()
+        resnet = models.resnet50(pretrained=True)
+        for param in resnet.parameters():
+            param.requires_grad_(False)
+        
+        modules = list(resnet.children())[:-1]
+        self.resnet = nn.Sequential(*modules)
+        self.embed = nn.Linear(resnet.fc.in_features, embed_size)
+
+    def forward(self, images):
+        features = self.resnet(images)
+        features = features.view(features.size(0), -1)
+        features = self.embed(features)
+        return features
+    
+
+class DecoderRNN(nn.Module):
+    def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
+        super(DecoderRNN, self).__init__()
+        self.embed = nn.Embedding(vocab_size, embed_size)
+        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers, batch_first=True)
+        self.linear = nn.Linear(hidden_size, vocab_size)
+    
+    def forward(self, features, captions):
+        embeddings = torch.cat((features.unsqueeze(1), self.embed(captions[:,:-1])), 1)
+        hiddens, _ = self.lstm(embeddings)
+        outputs = self.linear(hiddens)
+        return outputs
+
+    def sample(self, inputs, states=None, max_len=20):
+        " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
+        output_ids = []
+        for i in range(20):                                    # maximum sampling length
+            hiddens, states = self.lstm(inputs, states)        # (batch_size, 1, hidden_size), 
+            outputs = self.linear(hiddens.squeeze(1))
+            predicted = outputs.max(1)[1]
+            output_ids.append(predicted.tolist()[0])
+            inputs = self.embed(predicted)
+            inputs = inputs.unsqueeze(1)                       # (batch_size, 1, embed_size)
+        return output_ids
